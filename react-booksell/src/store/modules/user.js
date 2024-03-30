6c106d49ef406595b5e918b4bb43b38986a6c7cb
lgxssssss <127862134+lgxssssss@users.noreply.github.com>
2024-03-31 00:00:13 +0800

    封装API模块

--- react-booksell/src/store/modules/user.js
+++ react-booksell/src/store/modules/user.js
@@ -2,6 +2,7 @@
 import {createSlice} from '@reduxjs/toolkit'
 import { request } from '@/utils'
 import {setToken as _setToken , getToken,removeToken} from '@/utils'
+import { loginAPI,getProfileAPI } from '@/apis/user'
 
 const userStore = createSlice({
     name:"user",
@@ -40,7 +41,7 @@ const userReducer = userStore.reducer
 //异步方法  完成登陆获取token
 const fetchLogin = (loginForm) => {
     return async (dispatch) => {
-    const res = await request.post('/authorizations', loginForm)
+    const res = await loginAPI(loginForm)
     dispatch(setToken(res.data.token))
         }
     }
@@ -48,7 +49,7 @@ const fetchLogin = (loginForm) => {
     //获取个人信息异步方法
 const fetchUserInfo = () => {
     return async (dispatch) => {
-        const res = await request.get('/user/profile')
+        const res = await getProfileAPI()
         dispatch(setUserInfo(res.data))
         }
     }
