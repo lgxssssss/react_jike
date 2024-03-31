f1e81a26698b07cfec3af56d2ea38240169ecdd0
lgxssssss <127862134+lgxssssss@users.noreply.github.com>
2024-03-31 10:27:03 +0800

    频道列表的获取渲染

--- react-booksell/src/pages/Publish/index.js
+++ react-booksell/src/pages/Publish/index.js
@@ -15,11 +15,26 @@ import './index.scss'
 
  import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
+import { useEffect, useState } from 'react'
+import { getChannelAPI } from '@/apis/article'
   const { Option } = Select
 
 
   
   const Publish = () => {
+    //获取频道列表
+    const [channelList, setChannelList] = useState([])
+
+    useEffect(()=>{
+        //封装函数，在函数体内调用接口
+        const getChannelList = async ()=>{
+            const res = await getChannelAPI()
+            setChannelList(res.data.channels)
+        }
+        //调用函数
+        getChannelList()
+    },[])
+
     return (
       <div className="publish">
         <Card
@@ -49,7 +64,8 @@ import 'react-quill/dist/quill.snow.css'
               rules={[{ required: true, message: '请选择文章频道' }]}
             >
               <Select placeholder="请选择文章频道" style={{ width: 400 }}>
-                <Option value={0}></Option>
+                {/* value属性用户选中后会自动收集起来作为接口提交字段 */}
+                {channelList.map(item=><Option key={item.id} value={item.id}>{item.name}</Option>)}
               </Select>
             </Form.Item>
             <Form.Item
