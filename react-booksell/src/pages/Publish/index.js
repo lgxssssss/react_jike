2a5c233afd3aea8a7412b1a839638a45ab4fdcb7
lgxssssss <127862134+lgxssssss@users.noreply.github.com>
2024-04-01 14:33:42 +0800

    携带封面图发布文章

--- react-booksell/src/pages/Publish/index.js
+++ react-booksell/src/pages/Publish/index.js
@@ -7,7 +7,8 @@ import {
     Input,
     Upload,
     Space,
-    Select
+    Select,
+    message
   } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
@@ -38,6 +39,8 @@ import { createArticleAPI, getChannelAPI } from '@/apis/article'
     //提交表单
     const onFinish = (formValue) => {
         console.log(formValue)
+        // 校验封面类型imageType是否和实际的图片列表imageList数量是相等的
+        if (imageList.length !== imageType) return message.warning('封面类型和图片数量不匹配')
         const {title, content, channel_id} = formValue
 
         // 1. 按照接口文档的格式处理收集到的表单数据
@@ -45,8 +48,8 @@ import { createArticleAPI, getChannelAPI } from '@/apis/article'
             title,
             content,
             cover: {
-              type: 0, 
-              images:[]
+              type: imageType, //封面模式
+              images:imageList.map(item => item.response.data.url) //图片列表
             },
             channel_id
           }
